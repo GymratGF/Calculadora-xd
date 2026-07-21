60b8c3be7e1e33d4db1233bffe2c8d70d89809f3
Byron Gutiérrez <GymratGF>
2026-07-21 16:14:17 -0600

    codigo actualizado por errores

--- app.js
+++ app.js
@@ -1,13 +1,9 @@
-function  agregar(valor) {
+function  agregarNumero(valor) {
     document.getElementById("pantalla").value += valor;
 }
-function agregarOperacion(operacion) {
-    document.getElementById("pantalla").value += operacion;
-}
-function calcularResultado(igual) {
-    document.getElementById("pantalla").value += igual;
-    calcular();
-}
+
+
+
 function calcular() {
 try {
     document.getElementById("pantalla").value = eval(document.getElementById("pantalla").value); 
@@ -15,6 +11,8 @@ try {
 } catch (error) {
     document.getElementById("pantalla").value = "Error";
 }
+
+
 }
 function borrarTodo () {
     document.getElementById("pantalla").value = "";
@@ -22,3 +20,4 @@ function borrarTodo () {
 function borrarUno () {
     document.getElementById("pantalla").value = document.getElementById("pantalla").value.slice(0, -1);
 }
+//codigo actualizado
